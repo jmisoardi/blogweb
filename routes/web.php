c225aa84db6529fc303ba00fc861c5d9c147bf0a
jair Isoardi <jmisoardi@hotmail.com>
2024-12-29 04:43:07 -0300

    Ruta de prueba para verificar que la configuración de Laravel está funcionando correctamente.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta de prueba para verificar que la configuración de Laravel está funcionando correctamente. Para ello, vamos a crear una nueva ruta en el archivo routes/web.php. Para ello, vamos a crear una nueva ruta en el archivo routes/web.php.
+Route::get('/prueba', function () {
+    return ('¡Esta es una nueva ruta funcionando!');
+});
