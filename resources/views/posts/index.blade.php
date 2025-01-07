15fdb5e8102d8bc2db814029a188ea2e61735b81
jair Isoardi <jmisoardi@hotmail.com>
2025-01-06 23:01:27 -0300

    Utilizando Flowbite, pagina de diseño, implementando Alert en index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>Laravel 11 | Post</title>
+    
+    {{-- Esto es el CDN de Tailwind CSS para poder utilizar el framework --}}
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
-<body>
-    <h1>¡Usted señor jair esta en el inicio! </h1>
-</body>
+
+
+    <body>
+
+        <div class="max-w-4xl mx-auto px-4">
+            {{-- <h1>¡Usted señor jair esta en el inicio! </h1> --}}
+            
+            <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+            </div>
+
+        </div>
+
+    </body>
+
+
 </html>
